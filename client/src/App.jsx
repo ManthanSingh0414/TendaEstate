8ef59e2a5a92a571eeb561f514bed35c61f75763
Manthan <manthansingh0414@gmail.com>
2024-07-01 17:49:18 +0530

    final commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,11 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Profile from "./pages/Profile"
 import Header from "./components/Header"
-
-
+import PrivateRoute from "./components/PrivateRoute"
+import CreateListing from "./pages/CreateListing"
+import UpdateListing from "./pages/UpdateListing"
+import Listing from "./pages/Listing"
+import Search from "./pages/Search"
 
 export default function App() {
   return (
@@ -17,7 +20,15 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/search" element={<Search />} />
+
+        <Route path="/listing/:listingId" element={<Listing />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/update-listing/:listingId" element={<UpdateListing />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
